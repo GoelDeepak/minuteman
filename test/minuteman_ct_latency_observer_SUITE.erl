aaf9972ae6d6fa11b589d25f91934428a7cfcfe5
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-09-09 16:21:43 -0700

    fixed mocks

--- test/minuteman_ct_latency_observer_SUITE.erl
+++ test/minuteman_ct_latency_observer_SUITE.erl
@@ -6,35 +6,21 @@ all() ->
       [test_init].
 
 init_per_testcase(_, Config) ->
+  mock_iptables(),
   application:ensure_all_started(minuteman),
-  %	{ok, Pid} = minuteman_ct_latency_observer:start_link(),
-	% [{pid, Pid} | Config].
   Config.
 
-end_per_testcase(_, Config) ->
+end_per_testcase(_, _Config) ->
   application:stop(minuteman).
-%  Pid = ?config(pid, Config),
-%  unlink(Pid),
-%  exit(Pid, shutdown),
-%  wait_for_death(Pid).
-
-% wait_for_death(Pid) -> wait_for_death(Pid, is_process_alive(Pid)).
-% 
-% wait_for_death(Pid, true) ->
-%   timer:sleep(10),
-%   wait_for_death(Pid);
-% wait_for_death(_Pid, false) -> ok.
 
 test_init(_Config) -> ok.
-%  Pid = ?config(pid, Config).
 
 mock_iptables() ->
-  Parent = self(),
   meck:new(iptables, [passthrough, no_link]),
-  meck:expect(io, check, fun(Table, Chain, Rule) ->
+  meck:expect(iptables, check, fun(_Table, _Chain, _Rule) ->
     {ok,[]}
   end),
-  meck:expect(io, insert, fun(Table, Chain, Rule) ->
+  meck:expect(iptables, insert, fun(_Table, _Chain, _Rule) ->
     {ok,[]}
   end).
 
