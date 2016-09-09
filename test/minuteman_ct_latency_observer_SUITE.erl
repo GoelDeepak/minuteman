c8dc7738900616a38cf2d3350437bdd1fbba1124
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-09-09 15:48:13 -0700

    runs!

--- test/minuteman_ct_latency_observer_SUITE.erl
+++ test/minuteman_ct_latency_observer_SUITE.erl
@@ -6,21 +6,35 @@ all() ->
       [test_init].
 
 init_per_testcase(_, Config) ->
-	{ok, Pid} = minuteman_ct_latency_observer:start_link(),
-	[{pid, Pid} | Config].
+  application:ensure_all_started(minuteman),
+  %	{ok, Pid} = minuteman_ct_latency_observer:start_link(),
+	% [{pid, Pid} | Config].
+  Config.
 
 end_per_testcase(_, Config) ->
-  Pid = ?config(pid, Config),
-  unlink(Pid),
-  exit(Pid, shutdown),
-  wait_for_death(Pid).
+  application:stop(minuteman).
+%  Pid = ?config(pid, Config),
+%  unlink(Pid),
+%  exit(Pid, shutdown),
+%  wait_for_death(Pid).
 
-wait_for_death(Pid) -> wait_for_death(Pid, is_process_alive(Pid)).
+% wait_for_death(Pid) -> wait_for_death(Pid, is_process_alive(Pid)).
+% 
+% wait_for_death(Pid, true) ->
+%   timer:sleep(10),
+%   wait_for_death(Pid);
+% wait_for_death(_Pid, false) -> ok.
 
-wait_for_death(Pid, true) ->
-      timer:sleep(10),
-      wait_for_death(Pid);
-wait_for_death(Pid, false) -> ok.
+test_init(_Config) -> ok.
+%  Pid = ?config(pid, Config).
 
-test_init(Config) ->
-  Pid = ?config(pid, Config).
+mock_iptables() ->
+  Parent = self(),
+  meck:new(iptables, [passthrough, no_link]),
+  meck:expect(io, check, fun(Table, Chain, Rule) ->
+    {ok,[]}
+  end),
+  meck:expect(io, insert, fun(Table, Chain, Rule) ->
+    {ok,[]}
+  end).
+
