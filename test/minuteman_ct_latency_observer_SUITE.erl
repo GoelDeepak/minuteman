173e99200c80efda5ef08a202006bde8914ea16a
Anatoly Yakovenko <aeyakovenko@gmail.com>
2016-09-12 11:55:19 -0700

    ct_slave/start/stop

--- test/minuteman_ct_latency_observer_SUITE.erl
+++ test/minuteman_ct_latency_observer_SUITE.erl
@@ -17,22 +17,14 @@ test_update_vip_names(_Config) ->
   #metrics{} = minuteman_ct_latency_observer:update_vip_names(#metrics{}).
 
 init_per_testcase(_, Config) ->
-  Pid = rpc:pmap({ct_slave, start}, [], minuteman_ct_latency_observer_SUITE),
+  {ok, Pid} = ct_slave:start(minuteman_ct_latency_observer_SUITE),
   Config1 = rpc:call(Pid, minuteman_ct_latency_observer_SUITE, init_node, [Config]),
   [{pid, Pid} | Config1].
 
 end_per_testcase(Config) ->
   Pid = ?config(pid, Config),
   ok = rpc:call(Pid, minuteman_ct_latency_observer_SUITE, end_node, [Config]),
-  unlink(Pid),
-  exit(Pid, shutdown),
-  wait_for_death(Pid).
-
-wait_for_death(Pid) -> wait_for_death(Pid, is_process_alive(Pid)).
-wait_for_death(Pid, true) ->
-  timer:sleep(10),
-  wait_for_death(Pid);
-wait_for_death(_Pid, false) -> ok.
+  ct_slave:stop(Pid).
 
 init_node(Config) ->
   mock_iptables_start(),
